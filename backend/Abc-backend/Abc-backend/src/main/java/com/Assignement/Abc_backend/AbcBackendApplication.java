6bae10315a006f5de6ebdecb6a8a34cfec849460
Sawdha2003 <sawdhamufeedh@gmail.com>
2024-07-21 19:38:14 +0530

    Backend of the Abc

---
+++ backend/Abc-backend/Abc-backend/src/main/java/com/Assignement/Abc_backend/AbcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Assignement.Abc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcBackendApplication.class, args);
+	}
+
+}
---
+++ backend/Abc-backend/Abc-backend/src/test/java/com/Assignement/Abc_backend/AbcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Assignement.Abc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
